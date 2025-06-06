90a3953d420bca6e62bd1c5c71c5599692b5bd47
danlakelake <danlakelake@gmail.com>
2025-06-06 17:42:19 -0600

    configuración de Tailwind finalizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: ["./templates/**/*.twig", "./src/**/*.{js,css}"],
+  content: [
+    "./templates/**/*.twig",   // templates Twig
+    "./src/**/*.{js,css}",     // JS y CSS fuente
+    "./web/**/*.twig",         // Twig en web/
+    "./web/**/*.js"            // JS en web/
+  ],
   theme: {
     extend: {},
   },
